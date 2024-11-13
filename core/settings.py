361710958eef0e55dc07d9e0943b04efa5955f93
karem <karemmagdy247@gmail.com>
2024-11-13 21:32:39 +0200

    update

--- core/settings.py
+++ core/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "verceldb",
+        "USER": "default",
+        "PASSWORD": "aNdCngbe0W9S",
+        "HOST": "ep-solitary-truth-13080581.us-east-1.aws.neon.tech",
+        "PORT": "5432",
     }
 }
 
