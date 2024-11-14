c97c1175c7ae93758a1db1e66d5b1f9b1879575d
karem <karemmagdy247@gmail.com>
2024-11-14 15:40:01 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -160,7 +160,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
