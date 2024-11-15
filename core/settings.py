f8da52acbd7e036df0ae10b813ad18d06b62d52a
karem <karemmagdy247@gmail.com>
2024-11-15 21:31:14 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,7 @@ SIMPLE_JWT = {
     "JSON_ENCODER": None,
     "JWK_URL": None,
     "LEEWAY": 10,
-    "AUTH_HEADER_TYPES": ("Bearer", "JWT"),
+    "AUTH_HEADER_TYPES": ("JWT",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
     "USER_ID_FIELD": "id",
     "USER_ID_CLAIM": "user_id",
