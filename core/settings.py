d4927515f859ea77d74bae149c897d33e82cdba6
karem <karemmagdy247@gmail.com>
2024-11-17 00:14:12 +0200

    update

--- core/settings.py
+++ core/settings.py
@@ -127,12 +127,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://math-society.vercel.app",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://math-society.vercel.app",
+]
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
 
 # SIMPLE_JWT = {
