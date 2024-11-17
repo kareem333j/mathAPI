0f42c4597662e00cded098bbaddfccfc9155753d
karem <karemmagdy247@gmail.com>
2024-11-18 00:02:40 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -192,7 +192,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
